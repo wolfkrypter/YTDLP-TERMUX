e827472fba4b6e536d3ef5922aa349212999e248
wolfkrypter <zenhelbour@gmail.com>
2024-12-06 16:21:12 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 echo -e "\e[0;33m" "\n\nYT-DLP-TERMUX\n\n\n\n"
+echo -e "\e[0;32m" "\nInf/Exit/Cancel/Logout:\e[0;33m CTRL+C"
 echo -e "\e[0;35m" "\n\nINF:\n\n"
 echo -e "\e[0;32m" "YT-DLP-TERMUX ejecuta de forma legible o para un público general el extractor multiplaforma de urlstream YT-DLP\n\n\n"
 
@@ -13,8 +14,8 @@ cd /storage/emulated/0/Download
 
 echo -e "\e[0;33m" "\n\nEjemplo/URL: \n\n"
 echo -e "\e[0;32m" "https://m.youtube.com/watch?v=RgKAFK5djSk\n"
-echo -e "\e[0;33m" "\n\nPlataformas: Facebook,Youtube,Instagram,etcétera."
-echo -e "\e[0;33m" "\n\n\nCopie la url de la plataforma de streaming que desea extraer y codificar en un formato almacenable u legible de Video y pegue en URL:\n\n"
+echo -e "\e[0;31m" "\n\nPlataformas:\e[0;33m Facebook,Youtube,Instagram,etcétera."
+echo -e "\e[0;31m" "\n\n\n[INF/YTDLP-TERMUX]\e[0;33m\n\n\nCopie la url de la plataforma de streaming que desea extraer y codificar en un formato almacenable u legible de Video y pegue en URL.\n\n"
 echo -e "\e[0;35m" "\n\nURL:"
 read u
 
@@ -47,11 +48,13 @@ yt-dlp -f $v+$a --no-keep-video $u
 
 
 
-echo -e "\e[0;35m" "\n\n\n[Descarga/finalizada]\n\n\n"
+echo -e "\e[0;35m" "\n\n\n[Proceso/finalizado]\n\n\n"
 
+echo -e "\e[0;35m" "[Inf/YTDLP-TERMUX]"
+echo -e "\e[0;33m" "\n\n=> Si la extracción fue exitosa, omita reintentar de nuevo ante un posible error.\n\n=> Si ocurre un error y finalizo el proceso sin obtener la extracción de la urlstream en formato almacenable en el directorio de DESCARGAS por defecto, verifique su conexión, la url, y constate los IDS de audio y video ingresados ante tal error, y reintente nuevamente, si ocurre errores que requieran autenticación de cuenta o usuario, o omita esa plataforma, e intente con otra plataforma de streaming"
 
-echo -e "\e[0;33m" "\nFrase de gracia, devoción y fe:"
+echo -e "\e[0;33m" "\n\n\nFrase de gracia, devoción y fe:"
 
 echo -e "\e[0;34m" "\n\nJesucristo viene pronto"
  echo -e "\e[0;31m" "\n\nMateo 24:25, Author: Belial;\n\n\n"
- cd /data/data/com.termux/files/home/YTDLP-TERMUX
+cd /data/data/com.termux/files/home/YTDLP-TERMUX

75a805e5d08fc98daf9e26c5708ea7cbf2e3c110
wolfkrypter <zenhelbour@gmail.com>
2024-12-06 11:46:28 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,8 +10,9 @@ cd /data/data/com.termux/files/home/storage/downloads
 echo -e "\e[0;35m" "\n\n[YT-DLP-TERMUX/EJECUTANDOSE]\n\n"
 
 echo -e "\e[0;33m" "\n\nEjemplo/URL: \n"
+echo -e "\e[0;33m" "\n\nPlataformas: Facebook,Youtube,Instagram,etcétera."
 echo -e "\e[0;32m" "https://m.youtube.com/watch?v=RgKAFK5djSk\n\n\n\n"
-echo -e "\n\n\nCopie y pegue en URL:\n\n"
+echo -e "\n\n\nCopie la url de la plataforma de streaming que desea extraer y codificar en un formato almacenable u legible de Video y pegue en URL:\n\n"
 echo -e "\e[0;35m" "\n\nURL:"
 read url
 
@@ -24,7 +25,7 @@ echo -e "\e[0;32m" "\n\nDeslice hacía arriba e observe e elija el ID con la res
 
 echo -e "\e[0;35m" "\n\n[INFORMACIÓN/IDS | LISTA => FORMATOS]"
 
-echo -e "\e[0;32m" "\n\n\nLos IDs de los formatos de audio y video se encuentran resaltados en verde con el tamaño de archivo en blanco cuando se encuentran disponibles, y en un orden de calidad de arriba a hacía abajo de menor a mayor, o de baja a media y alta calidad.\n\n\n"
+echo -e "\e[0;32m" "\n\n\nLos IDs de los formatos de audio y video se encuentran resaltados en verde con el tamaño de archivo en blanco, y en un orden de calidad de arriba a hacía abajo de menor a mayor, o de baja a media y alta calidad.\n\n\n"
 
 echo -e "\e[0;31m" "\n\n\nEjemplo | ID/VIDEO: 137\n"
 

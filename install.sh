5104f55a15694f5f4b0b2db2a9df239c2f4d0478
wolfkrypter <zenhelbour@gmail.com>
2024-12-08 11:12:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo -e "\e[034m" "\n\nInstalando FFMPEG dependencia de YT-DLP\n\n"
 yes | pkg install ffmpeg
 
 echo -e "\e[034m"  "\n\nInstalando YT-DLP\n\n"
-pip install yt-dlp
+yes | pip install yt-dlp
 
 echo -e "\e[0;31m" "\n\nFinalizo el proceso de instalación, para utilizar los servicios ofrecidos de YT-DLP en YTDLP-TERMUX visualice con el comando 'cat README.md' en el directorio del repositorio clonado YTDLP-TERMUX, ejemplo: '~ $ cat README.md', e utilice según su preferencia.\n\n"
 

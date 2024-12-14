0dfff7b9b8ac1fca359b2153dc1ca72d359db3b2
wolfkrypter <zenhelbour@gmail.com>
2024-12-14 11:32:10 -0500

    Actualizar Start-audio.sh

--- Start-audio.sh
+++ Start-audio.sh
@@ -21,13 +21,13 @@ echo -e "\e[0;32m" "\n\n\n INF/YTDLP-TERMUX: YT-DLP"
 
 echo -e "\e[0;32m" "\n\n\n\nInf/Exit/Cancel/Logout:\e[0;33m CTRL+C"
 echo -e "\e[0;35m" "\n\nINF:\n\n"
-echo -e "\e[0;32m" "YT-DLP-TERMUX ejecuta de forma legible o para un público general el extractor multiplaforma de urlstream YT-DLP\n\n\n"
+echo -e "\e[0;32m" "YTDLP-TERMUX ejecuta de forma legible o para un público general el extractor multiplaforma de urlstream YT-DLP\n\n\n"
 
 echo -e "\e[0;33m" "\n\n\nAlmacenamiento de Audio:\n\n\n"
-echo -e "\e[0;32m" "\n\nYT-DLP-TERMUX dirige todas las descargas directamente al directorio de DESCARGAS automáticamente\n\n\n"
+echo -e "\e[0;32m" "\n\nYTDLP-TERMUX dirige todas las descargas directamente al directorio de DESCARGAS automáticamente\n\n\n"
 yes | termux-setup-storage
 
-echo -e "\e[0;35m" "\n\n[YT-DLP-TERMUX/EJECUTANDOSE]\n\n"
+echo -e "\e[0;35m" "\n\n[YTDLP-TERMUX/EJECUTANDOSE]\n\n"
 sleep 2
 cd /storage/emulated/0/Download
 

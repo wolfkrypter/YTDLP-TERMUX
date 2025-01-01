45ca288ab4e03c8c6eef97d3711036085f60c782
wolfkrypter <zenhelbour@gmail.com>
2025-01-01 10:08:52 -0500

    Actualizar uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -32,12 +32,7 @@ echo -e "\n\nFinalizo el proceso de desinstalación"
 echo -e "\033[0;49;32m[\033[0mINF\033[0;49;32m/\033[0mYTDLP-TERMUX\033[0;49;32m]\033[0m"
 
 
-echo -e "La eliminación total de YTDLP-TERMUX, es llevada a cabo claramente por el usuario, al eliminar claro esta mediante instructivos predeterminados para este proceso con antelación, de la forma preestablecida, en README, el cual instruirá el procedimiento final para la eliminación total de YTDLP-TERMUX en TERMUX,etcétera,"
-
-
-
-
-echo -e "por eso es necesario que se dirija a README para culminar la desinstalación total de YTDLP-TERMUX visualizando README, claro está de forma automática e automáticamente para agilizar el procedimiento de desinstalación de forma más versátil de YTDLP-TERMUX, el cuál es posible hasta este punto posiblemente de forma automatizada por imposibilitación claro está de forma predeterminada por razones de ubicaciones predeterminadas y operaciones no posibles de forma automatizada por imposibilitaciones auto-ejecutables e ejecutables que requerirían presencialidad, por esa razón es realizada la desinstalación de YTDLP-TERMUX de esta forma."
+echo -e "La eliminación total de YTDLP-TERMUX, es llevada a cabo claramente por el usuario, al eliminar claro esta mediante instructivos predeterminados para este proceso con antelación, de la forma preestablecida, en README, el cual instruirá el procedimiento final para la eliminación total de YTDLP-TERMUX en TERMUX,etcétera, por eso es necesario que se dirija a README para culminar la desinstalación total de YTDLP-TERMUX visualizando README, claro está de forma automática e automáticamente para agilizar el procedimiento de desinstalación de forma más versátil de YTDLP-TERMUX, el cuál es posible hasta este punto posiblemente de forma automatizada por imposibilitación claro está de forma predeterminada por razones de ubicaciones predeterminadas y operaciones no posibles de forma automatizada por imposibilitaciones auto-ejecutables e ejecutables que requerirían presencialidad, por esa razón es realizada la desinstalación de YTDLP-TERMUX de esta forma."
 
 
 

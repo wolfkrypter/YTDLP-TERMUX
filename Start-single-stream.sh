5a6ff7756e97a52117c03c093df4d3e6f9f6bff6
wolfkrypter <zenhelbour@gmail.com>
2025-01-01 10:38:07 -0500

    Actualizar Start-single-stream.sh

--- Start-single-stream.sh
+++ Start-single-stream.sh
@@ -14,15 +14,15 @@ echo -e "\033[1;49;31m #########################################################
 
 
 
-echo -e "\033[0;49;32m\n\n\n INF/YTDLP-TERMUX:\033[0mYT-DLP"
+echo -e "\033[0;49;32m\n\n\nINF/YTDLP-TERMUX:\033[0mYT-DLP"
 
 
 
 
 
 
-echo -e "\033[0;49;32m\n\n\n\n Inf/Exit/Cancel/Logout:\033[0m CTRL+C"
-echo -e "\033[0;49;32m\n\n INF/YTDLP-TERMUX:\n\n\033[0m"
+echo -e "\033[0;49;32m\n\n\n\nInf/Exit/Cancel/Logout:\033[0m CTRL+C"
+echo -e "\033[0;49;32m\n\nINF/YTDLP-TERMUX:\n\n\033[0m"
 echo -e "YTDLP-TERMUX ejecuta de forma legible o para un público general el extractor de urlstream YT-DLP\n\n\n"
 
 
@@ -35,15 +35,15 @@ sleep 1
 echo -e "\n\033[0;49;32m[INFORMACIÓN/ALMACENAMIENTO]\033[0m\n\nLas descargas van directamente al directorio de DESCARGAS del almacenamiento interno de su dispositivo automáticamente.\n\n"
 
 
-echo -e "\033[0;49;32m\n\n Ejemplo/URL: \n\n\033[0m"
+echo -e "\033[0;49;32m\n\nEjemplo/URL: \n\n\033[0m"
 echo -e "https://www.facebook.com/100066604305115/videos/3906285049647051/?mibextid=rS40aB7S9Ucbxw6v\n"
 
-echo -e "\033[0;49;32m\n\n Plataformas:\033[0m Facebook,Instagram,etcétera."
+echo -e "\033[0;49;32m\n\nPlataformas:\033[0m Facebook,Instagram,etcétera."
 
 echo -e "\033[0;49;32m\n\n\n[INF/YTDLP-TERMUX]\033[0m\n\nCopie la url single-streaming de la plataforma de streaming que desea extraer y codificar en un formato almacenable u legible de Video y pegue en URL.\n\n"
 
 
-echo -e "\033[0;49;32m\n\n URL:\033[0m"
+echo -e "\033[0;49;32m\n\nURL:\033[0m"
 read u
 
 
@@ -58,9 +58,9 @@ echo -e "\n\nDeslice hacía arriba e observe e elija el ID con la resolución y 
 
 
 
-echo -e "\033[0;49;32m\n\n\n Ejemplo | ID/URL:\033[0m 978692127419239v \033[0;49;32m | \033[0mhls-360p\n"
+echo -e "\033[0;49;32m\n\n\nEjemplo | ID/URL:\033[0m 978692127419239v \033[0;49;32m | \033[0mhls-360p\n"
 
-echo -e "\n\033[0;49;32m Inf/ID/URL:\033[0m"
+echo -e "\n\033[0;49;32mInf/ID/URL:\033[0m"
 echo -e "\nCopie y pegue el ID en ID/URL o digite.\n\n"
 echo -e "\033[0;49;32m\n\n ID/URL:\033[0m"
 read iv

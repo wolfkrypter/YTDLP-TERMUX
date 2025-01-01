150475e28ec84da50662828eeda19257d0207373
wolfkrypter <zenhelbour@gmail.com>
2025-01-01 10:17:16 -0500

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,32 @@
-echo -e "\033[0;49;32m\n\nInstalando  y actualizando paquetes y dependencias por defecto de Termux\n\n\033[0m"
+
+echo -e "\033[0;49;32m[\033[0mInstalación\033[0;49;32m/\033[0mDependencias\033[0;49;32m]\033[0m"
+
+echo -e "\n\nInstalando  y actualizando paquetes y dependencias por defecto de Termux\n\n"
 yes | apt update && yes | apt upgrade
 
-echo -e "\033[0;49;32m\n\nPermiso de acceso de almacenamiento a Termux automatizado ejecutado\n\n\033[0m"
+echo -e "\033[0;49;32m[\033[0mEjecución\033[0;49;32m/\033[0mTERMUX-ALMACENAMIENTO\033[0;49;32m]\033[0m"
+
+echo -e "\n\nPermiso de acceso de almacenamiento a Termux automatizado ejecutado\n\n\033[0m"
 sleep 2
 yes | termux-setup-storage 
 
-echo -e "\033[0;49;32m\n\nInstalando python y dependencias de python\n\n\033[0m"
+
+
+echo -e "\033[0;49;32m[\033[0mInstalación\033[0;49;32m/\033[0mPYTHON\033[0;49;32m]\033[0m"
+
+echo -e "\n\nInstalando python y dependencias de python\n\n"
 yes | pkg install python
 
-echo -e "\033[0;49;32m\n\nInstalando FFMPEG dependencia de YT-DLP\n\n\033[0m"
+
+
+echo -e "\033[0;49;32m[\033[0mInstalación\033[0;49;32m/\033[0mFFMPEG\033[0;49;32m]\033[0m"
+
+echo -e "\n\nInstalando FFMPEG dependencia de YT-DLP\n\n"
 yes | pkg install ffmpeg
+
+
+echo -e "\033[0;49;32m[\033[0mInstalación\033[0;49;32m/\033[0mYT-DLP\033[0;49;32m]\033[0m"
+
 
 echo -e "\033[0;49;32m\n\nInstalando YT-DLP\n\n\033[0m"
 yes | pip install yt-dlp
@@ -22,5 +39,10 @@ mv Start-duo-stream.sh /data/data/com.termux/files/usr/bin
 mv Start-audio-stream.sh /data/data/com.termux/files/usr/bin
 mv Start-single-stream.sh /data/data/com.termux/files/usr/bin
 
-echo -e "\033[0;49;32m\n\nFinalizo el proceso de instalación, para utilizar los servicios ofrecidos de YT-DLP en YTDLP-TERMUX visualice con el comando 'cat README.md' en el directorio del repositorio clonado YTDLP-TERMUX, ejemplo: '~ $ cat README.md', e utilice según su preferencia.\n\n\033[0m"
+
+
+echo -e "\033[0;49;32m[\033[0mFinalización\033[0;49;32m/\033[0mInstalación\033[0;49;32m]\033[0m"
+
+
+echo -e "\n\nFinalizo el proceso de instalación, para utilizar los servicios ofrecidos de YT-DLP en YTDLP-TERMUX visualice con el comando 'cat README.md' en el directorio del repositorio clonado YTDLP-TERMUX, ejemplo: '~ $ cat README.md', e utilice según su preferencia.\n\n"
 
